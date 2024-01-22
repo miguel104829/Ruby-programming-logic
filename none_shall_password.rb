90e5b3e3d2269c19ac16c745db6fe41da9692bc4
Miguel Samaca <miguel_samaca@hotmail.com>
2024-01-21 19:28:53 -0500

    Add logic options 1 and 2 of user selection

--- none_shall_password.rb
+++ none_shall_password.rb
@@ -1,6 +1,6 @@
 EMAIL = "samaca@example.com"
 PASSWORD = "12345"
-
+PASSWORD_VAULT = {aws: {username: "miguel", password: 54321}}
 puts "Welcome to None Shall Pass - A Pasword Manager"
 puts "Please sing in to continue"
 puts "Enter email: "
@@ -29,9 +29,26 @@ end
 
 case user_selection
 when "1"
-    puts "this will create new service credentials"
+    puts "Enter the name of the new service: "
+    new_service = gets.chomp
+    PASSWORD_VAULT[new_service] = {}
+    
+    print "Please enter the usename for this new service: "
+    new_service_username = gets.chomp
+    PASSWORD_VAULT[new_service]["username"] = new_service_username
+    
+    print "Please enter the password for this new service: "
+    new_service_password = gets.chomp
+    PASSWORD_VAULT[new_service]["password"] = new_service_password 
 when "2"
-    puts "this will retrieve service credentials"
+    print "Please enter the name of the of the service you wish to access credencials for: "
+    requested_service_name = gets.chomp
+    credentials = PASSWORD_VAULT[requested_service_name.to_sym]
+    
+    puts "Here are the credencials for #{requested_service_name}:"
+    credentials.each do |key, val|
+        puts "#{key}: #{val}"
+    end
 else
     puts "Exiting the program.Goodbye."
     exit
